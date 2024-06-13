53841ecdafd2adcc5b1fc241797bb5ee4e4885b2
HallowXIII <zephyrnox@gmail.com>
2024-06-13 14:59:15 +0200

    move to a connection pool again

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,12 @@
-import { Client } from 'pg'
+import { Pool } from 'pg';
 
-// Definitely change these. Your deployment environment should tell you what to do here.
-const CLIENT_PROPS = {
+// Create a pool with your database credentials
+const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.DATABASE_URL,
   password: process.env.PGPASSWORD,
   port: Number(process.env.PGPORT),
-}
+});
 
-export const client = new Client(CLIENT_PROPS)
+export default pool;
