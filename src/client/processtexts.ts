83d3bb20ad0935f75584b731b4cfc4274b2b2519
cdnilsen <campbell.nilsen@gmail.com>
2024-12-29 11:10:31 -0500

    debugging

--- src/client/processtexts.ts
+++ src/client/processtexts.ts
@@ -208,12 +208,12 @@ function isValidEdition(edition: string): edition is EditionName {
 
 
 function reprocessID(id: string, column: string): string {
-    let dict = {
+    const dict: { [key: string]: string } = {
         'first_edition': '2',
         'second_edition': '3',
         'mayhew': '4',
         'zeroth_edition': '5'
-    }
+    };
     
     if ((column in dict) && (id.length > 0)) {
         let strippedID = id.slice(1);
