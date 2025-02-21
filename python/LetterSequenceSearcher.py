2d27e76badb8ac4449ca72e0beaeab68fcd88187
cdnilsen <campbell.nilsen@gmail.com>
2025-02-21 11:59:53 -0500

    nehemiah 3

--- python/LetterSequenceSearcher.py
+++ python/LetterSequenceSearcher.py
@@ -18,17 +18,61 @@ def cleanLine(line):
     for char in punctuation:
         line = line.replace(char, "")
 
+    line = line.lower()
+
     return line.split(" ")
 
+def getAllLetters(word, numLetters):
+    allSequences = []
+    for i in range(len(word) - (numLetters - 1)):
+        allSequences.append(word[i:i + numLetters])
+    return allSequences
+
+
 def main(edition, numLetters, ignoreDiacritics = False):
+    alphabeticalOrByCount = input("Sort alphabetically (a) or by count (c)? ").strip().lower()
+
+    if alphabeticalOrByCount != "a" and alphabeticalOrByCount != "c":
+        return
+
     fileList = getFiles(edition)
 
-    allWords = []
-
+    sequenceToCountDict = {}
     for file in fileList:
         lines = open('../texts/' + file, "r", encoding="utf-8").readlines()
         for line in lines:
             words = cleanLine(line)
+            for word in words:
+                word = word.strip()
+                wordSequences = getAllLetters(word, numLetters)
+                for sequence in wordSequences:
+                    if sequence in sequenceToCountDict:
+                        sequenceToCountDict[sequence] += 1
+                    else:
+                        sequenceToCountDict[sequence] = 1
+        #print("Finished " + file) # This goes really fast, anyways
+
+    allSequences = list(sequenceToCountDict.keys())
+
+    if alphabeticalOrByCount == "a":
+        allSequences.sort()
+    else:
+        allSequences.sort(key=lambda x: sequenceToCountDict[x], reverse=True)
     
+    '''
+    for i in range (50):
+        print(allSequences[i] + ": " + str(sequenceToCountDict[allSequences[i]]))
+    '''
+    
+    outputFile = open("letterSequenceOutput.txt", "w", encoding="utf-8")
+    outputLines = []
+    for sequence in allSequences:
+        count = sequenceToCountDict[sequence]
+        sequenceLine = sequence + ": " + str(count) + "\n"
+        outputLines.append(sequenceLine)
+    outputFile.writelines(outputLines)
+
+    print("Found " + str(len(allSequences)) + " sequences")
+
 
 main("First Edition", 3)
