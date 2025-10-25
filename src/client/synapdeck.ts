837f8be81d770f845ffcd09e8c4340fd4b002254
cdnilsen <campbell.nilsen@gmail.com>
2025-10-25 07:16:19 -0400

    whoopsies...

--- src/client/synapdeck.ts
+++ src/client/synapdeck.ts
@@ -680,9 +680,11 @@ function cleanFieldDatum(card: CardDue, targetIndex: number, isBackOfCard: boole
         case "Ge'ez":
             output = transliterateGeez(datum, isBackOfCard);
             break;
+        /*
         case "Ancient Greek":
             output = transliterateGreek(datum);
             break;
+        */
         case "Hebrew":
             output = transliterateHebrew(datum, true);
             break;
