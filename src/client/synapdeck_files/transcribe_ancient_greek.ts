4696551a2485581208ecf2e6c3ea37ddbe3bb0cd
cdnilsen <campbell.nilsen@gmail.com>
2026-02-23 19:53:33 -0600

    cleaning up

--- src/client/synapdeck_files/transcribe_ancient_greek.ts
+++ src/client/synapdeck_files/transcribe_ancient_greek.ts
@@ -657,11 +657,7 @@ function isAlreadyGreek(str: string): boolean {
 }
 
 export function transliterateGreek(str: string): string {
-
-  console.log("🐦 CANARY - transliterateGreek");
-  const alreadyGreek = isAlreadyGreek(str);
-  console.log(`transliterateGreek: "${str}", isAlreadyGreek=${alreadyGreek}, codepoints:`, [...str].map(c => c.codePointAt(0)!.toString(16)));
-  
+  const alreadyGreek = isAlreadyGreek(str);  
   if (alreadyGreek) {
     return str.normalize("NFC");
   }
